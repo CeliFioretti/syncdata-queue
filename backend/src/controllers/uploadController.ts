957ed7a2c3b9d6330114d4dd83e0bb4c1279167a
CeliFioretti <celi.fioretti.12@gmail.com>
2026-04-25 10:36:30 -0300

    feat: agrega lógica de queue

--- backend/src/controllers/uploadController.ts
+++ backend/src/controllers/uploadController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { processExcelSync } from '../services/excelService';
+import { randomUUID } from 'crypto';
 
 export const uploadFile = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
@@ -9,13 +9,23 @@ export const uploadFile = async (req: Request, res: Response, next: NextFunction
     }
 
     const filePath = req.file.path;
+    const originalName = req.file.originalname;
 
-    const result = await processExcelSync(filePath);
+    // 1. Generamos el "Ticket" para el usuario
+    const jobId = randomUUID();
 
+    // TODO (Paso 3): Aquí le diremos a BullMQ "Tomá este filePath y agregalo a la cola"
+    // await queueService.addJob({ filePath, originalName, jobId });
+
+    // 2. Respondemos HTTP 202 (Accepted) inmediatamente
     res.status(200).json({
       status: 'success',
-      message: 'Archivo procesado e ingresado al sistema.',
-      data: result
+      message: 'Archivo recibido correctamente. El procesamiento en segundo plano ha comenzado.',
+      data: {
+        jobId,
+        fileName: originalName,
+        status: 'PENDING'
+      }
     });
   } catch (error) {
     next(error);
---
+++ backend/src/services/queueService.ts
@@ -0,0 +1,38 @@
+import { Queue } from 'bullmq';
+import IORedis from 'ioredis';
+
+// Conexión a Redis local
+const redisConnection = new IORedis({
+  host: '127.0.0.1',
+  port: 6379,
+  maxRetriesPerRequest: null, // Requisito para evitar deadlocks
+});
+
+// Instanciamos la Cola (Queue) llamada 'excel-processing'
+export const uploadQueue = new Queue('excel-processing', {
+  connection: redisConnection,
+  
+  // 🛡️ REGLAS DE SEGURIDAD Y RENDIMIENTO (TTL)
+  defaultJobOptions: {
+    removeOnComplete: {
+      age: 3600, 
+      count: 100, 
+    },
+    removeOnFail: {
+      age: 86400, 
+      count: 1000,
+    },
+    attempts: 3, 
+    backoff: {
+      type: 'exponential',
+      delay: 2000, // Espera 2s, luego 4s, luego 8s...
+    }
+  }
+});
+
+// Función exportable para agregar tickets a la cola
+export const addUploadJob = async (jobData: { filePath: string; originalName: string; jobId: string }) => {
+  await uploadQueue.add('process-excel', jobData, {
+    jobId: jobData.jobId,
+  });
+};
